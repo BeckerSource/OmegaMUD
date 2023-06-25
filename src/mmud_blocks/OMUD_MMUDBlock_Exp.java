b85826e837403c14e5242a6ae7f90e7f295ffce5
BeckerSource <git@github.com>
2023-06-25 11:56:21 -0400

    improved exp parsing

--- src/mmud_blocks/OMUD_MMUDBlock_Exp.java
+++ src/mmud_blocks/OMUD_MMUDBlock_Exp.java
@@ -18,15 +18,14 @@ public class OMUD_MMUDBlock_Exp extends OMUD_MMUDBlocks.Block{
 			cleanData(_sbBlockData, false, true);
 			mmc.dataExp = new OMUD_MMUD.DataExp();
 
-			int pos_left = 	0;
+			int pos_left =  0;
 			int pos_right = 0;
 			if ((pos_right = _sbBlockData.indexOf(MSTR_LEVEL, pos_left)) > -1){
 				mmc.dataExp.cur_total = Integer.parseInt(_sbBlockData.substring(pos_left, pos_right).trim());
-	
-				// just get the total for next level, cal the rest...
-				pos_left = pos_right;
-				if ((pos_left  = _sbBlockData.indexOf(MSTR_NEXT_TOTAL_PRE, pos_left)) > -1 &&
-					(pos_right = _sbBlockData.indexOf(MSTR_NEXT_TOTAL_END, pos_left)) > -1){
+
+				pos_right = _sbBlockData.length() - 1;
+				if ((pos_right = _sbBlockData.lastIndexOf(MSTR_NEXT_TOTAL_END, pos_right)) > -1 &&
+					(pos_left  = _sbBlockData.lastIndexOf(MSTR_NEXT_TOTAL_PRE, pos_right)) > -1){
 					mmc.dataExp.next_total = Integer.parseInt(_sbBlockData.substring(pos_left + MSTR_NEXT_TOTAL_PRE.length(), pos_right).trim());
 					mmc.dataExp.next_rem = mmc.dataExp.next_total - mmc.dataExp.cur_total;
 				}
