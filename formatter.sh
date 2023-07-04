1283f424b6dee36e3b4bebd594908d8e4b72fbb1
BeckerSource <git@github.com>
2023-07-04 14:24:18 -0400

    formatter update

--- formatter.sh
+++ formatter.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # convert tabs to 4-spaces (expand) and remove trailing line spaces...
-find ./src -type f -name '*.java' -exec bash -c \
-    ' \
+find ./src -type f -name '*.java' -exec bash -c ' \
     expand -t 4 "$0" > "$0.bak" && mv "$0.bak" "$0" && \
     sed -i "s/ *$//" $0 \
     ' {} \;
